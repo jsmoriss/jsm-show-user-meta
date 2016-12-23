91b50f987c4a62621c2de65d504a881a4a2f6654
JS Morisset <jsm@surniaulula.com>
2016-12-22 22:29:27 -0800

    v1.0.2-1

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -12,7 +12,7 @@
  * Description: Show all user meta (aka custom fields) keys and their unserialized values in a metabox on user profile editing pages.
  * Requires At Least: 3.7
  * Tested Up To: 4.7
- * Version: 1.0.2-dev1
+ * Version: 1.0.2-1
  *
  * Version Components: {major}.{minor}.{bugfix}-{stage}{level}
  *

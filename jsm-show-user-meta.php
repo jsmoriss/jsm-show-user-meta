4bac6c0ed0ff8a6a279e0d71301c32d921c74d73
JS Morisset <jsm@surniaulula.com>
2020-04-04 11:57:10 -0700

    v1.1.0

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -11,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Show all user meta (aka custom fields) keys and their unserialized values in a metabox on user profile editing pages.
  * Requires PHP: 5.6
- * Requires At Least: 4.0
+ * Requires At Least: 4.2
  * Tested Up To: 5.4
  * Version: 1.1.0
  *

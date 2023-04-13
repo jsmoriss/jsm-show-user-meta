061d87d5eb37c461f420caac2002a47ff3f1e114
JS Morisset <jsm@surniaulula.com>
2023-04-13 06:43:53 -0700

    v3.0.11

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Show user metadata in a metabox when editing users - a great tool for debugging issues with user metadata.
  * Requires PHP: 7.2
- * Requires At Least: 5.4
+ * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * Version: 3.0.11
  *

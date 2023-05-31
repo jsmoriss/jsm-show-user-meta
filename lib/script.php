07c249ec798f0f0775606e3f3aa5767abd38becc
JS Morisset <jsm@surniaulula.com>
2023-05-31 06:44:35 -0700

    v3.0.12

--- lib/script.php
+++ lib/script.php
@@ -45,7 +45,7 @@ if ( ! class_exists( 'JsmSumScript' ) ) {
 
 			// The version number should match the version in js/com/jquery-admin-page.js.
 			wp_register_script( 'sucom-admin-page', JSMSUM_URLPATH . 'js/com/jquery-admin-page.min.js',
-				$deps = array( 'jquery' ), '20230522', $in_footer = true );
+				$deps = array( 'jquery' ), '20230531', $in_footer = true );
 
 			wp_localize_script( 'sucom-admin-page', $admin_l10n, $this->get_admin_page_script_data() );
 

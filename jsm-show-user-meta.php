6f38fdf1412c656823e0e112354151642a79732c
JS Morisset <jsm@surniaulula.com>
2023-07-04 10:54:29 -0700

    v3.1.0-rc.1

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -13,7 +13,7 @@
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
  * Tested Up To: 6.2.2
- * Version: 3.1.0-b.1
+ * Version: 3.1.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *

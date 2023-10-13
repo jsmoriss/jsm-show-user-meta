ffb7d838e95fd4c57c53aba632c60422986f0bf3
JS Morisset <jsm@surniaulula.com>
2023-10-13 03:23:10 -0700

    v3.3.0

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -12,7 +12,7 @@
  * Description: Show user metadata in a metabox when editing users - a great tool for debugging issues with user metadata.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
- * Tested Up To: 6.3.1
+ * Tested Up To: 6.3.2
  * Version: 3.3.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]

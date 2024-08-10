edbcc695bf8f8a9bcc172ee0aefc09ce067efb5c
jsmoriss <jsm@surniaulula.com>
2024-08-10 23:03:46 +0000

    v4.3.0

--- lib/script.php
+++ lib/script.php
@@ -47,7 +47,7 @@ if ( ! class_exists( 'JsmSumScript' ) ) {
 			 * The version number should match the version in js/com/jquery-admin-page.js.
 			 */
 			wp_register_script( 'sucom-admin-page', JSMSUM_URLPATH . 'js/com/jquery-admin-page.min.js',
-				$deps = array( 'jquery' ), '20230704', $in_footer = true );
+				$deps = array( 'jquery' ), '20240810', $in_footer = true );
 
 			wp_localize_script( 'sucom-admin-page', $admin_l10n, $this->get_admin_page_script_data() );
 

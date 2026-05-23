fa1af41cce0f2a34504f532576d616eee38d61b9
jsmoriss <jsm@surniaulula.com>
2026-05-23 17:56:16 +0000

    v4.9.0-rc.1

--- jsm-show-user-meta.php
+++ jsm-show-user-meta.php
@@ -13,7 +13,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
  * Tested Up To: 7.0
- * Version: 4.8.0
+ * Version: 4.9.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
